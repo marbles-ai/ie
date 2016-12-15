a91b495a334c3751b1c55682b0683f7021d5ba4f
Paul Glendenning <paul@subjectlens.com>
2016-12-15 12:19:11 -0800

    Add deps for OSX.

---
+++ deps/install-osx-brew-pkgs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function brew_install() {
+	brew list $1 >/dev/null 2>&1 && return 0
+	echo "Installing $1"
+	echo brew install $*
+	brew install $*
+}
+
+# Homebrew installs are local to the current user
+brew update
+brew_install coreutils
+brew_install doxygen
+
+# gRPC
+brew_install openssl
+brew_install maven
+brew_install gradle
+
+# pseudo terminal
+brew tap homebrew/dupes
+brew_install screen 
