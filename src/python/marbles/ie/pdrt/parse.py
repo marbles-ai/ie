57d97a8aaa7c42d5dc903b3bdeb767c04a59940b
Paul Glendenning <paul@subjectlens.com>
2016-12-15 09:09:02 -0800

    Add DRS parser. Improve pydoc for parser.

--- src/python/marbles/ie/pdrt/parse.py
+++ src/python/marbles/ie/pdrt/parse.py
@@ -1,24 +1,32 @@
+# NOTE: pypeg2 is written in python 3. The future import will convert all strings
+# to unicode.
 from __future__ import unicode_literals, print_function
 from pypeg2 import *
 import re
 from .drs import DRSRelation
-from .pdrs import MAP, PDRS, PDRSRef, PRef
+from .pdrs import MAP, PDRS, LambdaPDRS, PDRSRef, PRef, LambdaPDRSRef
 from .pdrs import PCond, PNeg, PRel, PProp, PImp, POr, PDiamond, PBox
+from .drs import DRS, DRSRef,  LambdaDRS, LambdaDRSRef
+from .drs import Neg, Rel, Prop, Imp, Or, Diamond, Box
 
-# Besides directly defining a PDRS using the PDRS syntax, it is also
-# possible to use a set-theoretical string input format directly.
+
+###########################################################################
+# Common to both parsers.
 #
 # The following names can be used for different operators (these are all
 # case insensitive):
 #
 # * Negation operators: !, not, neg
 # * Implication operators (infix): imp, ->, =>, then
-# * Disjuction operators (infix): v, or
+# * Disjunction operators (infix): v, or
 # * Box operators: b, box, necessary
 # * Diamond operators: d, diamond, maybe.
 #
-# "The man is not happy."
-# "<1,{},{ (1,not <5,{(2,x)},{(2,man(x)),(5,happy(x))},{(5,2)}>) },{}>"
+# Syntax:
+# * A DRS/PDRS is contained between '<' and '>'
+# * A list is contained between '{' and '}'. List can have [0,inf] elements
+# * A tuple is contained between '(' and ')'. Tuples have fixed cardinality.
+# * Elements are separated by a comma
 
 
 class NegateOp(Keyword):
@@ -26,28 +34,37 @@ class NegateOp(Keyword):
 
 
 class BinaryOp(Keyword):
-    grammar = Enum(K('b'), K('box'), K('necessary'), K('d'), K('diamond'), K('maybe'))
+    grammar = Enum(K('b'), K('box'), K('necessary'),
+                   K('d'), K('diamond'), K('maybe'),
+                   K('imp'), K('=>'), K('->'),
+                   K('or'), K('v'))
 
 
 Predicate = re.compile(r'[a-zA-Z][_\w]*')
-Number = re.compile(ur'-?\d+')
-PosInt = re.compile(ur'\d+')
+Number = re.compile(r'-?\d+')
+PosInt = re.compile(r'\d+')
+
+###########################################################################
+# PDRS Parser
 
 
 class Map(List):
     grammar = '(', Number, ',', Number, ')'
+
     def to_drs(self):
         return MAP(int(self[0]), int(self[1]))
 
 
 class ProjRef(List):
     grammar = '(', PosInt, ',', Predicate, ')'
+
     def to_drs(self):
         return PRef(int(self[0]), PDRSRef(self[1].encode('utf-8')))
 
 
-class RelExpr(List):
+class PRelExpr(List):
     grammar = Predicate, '(', csl(Predicate), ')'
+
     def to_drs(self):
         refs = [PDRSRef(r) for r in self[1].encode('utf-8')]
         return PRel(DRSRelation(self[0].encode('utf-8')), refs)
@@ -56,56 +73,94 @@ class RelExpr(List):
 class PdrsDecl(List):
     # Grammar is recursive so must declare with None
     grammar = None
+
     def to_drs(self):
         return PDRS(int(self[0]), self[3].to_drs(), self[1].to_drs(), self[2].to_drs())
 
 
-class NegExpr(List):
+class PNegExpr(List):
     grammar = NegateOp, PdrsDecl
+
     def to_drs(self):
         return PNeg(self[1].to_drs())
 
 
-class BinaryExpr(List):
+class PBinaryExpr(List):
     grammar = PdrsDecl, BinaryOp, PdrsDecl
+
     def to_drs(self):
-        if self[1] in ['d', 'diamond','maybe']:
+        if self[1] in ['d', 'diamond', 'maybe']:
             return PDiamond(self[0].to_drs(), self[2].to_drs())
-        else:
+        elif self[1] in ['b', 'box', 'necessary']:
             return PBox(self[0].to_drs(), self[2].to_drs())
+        elif self[1] in ['imp', '=>', '->']:
+            return PImp(self[0].to_drs(), self[2].to_drs())
+        else: # Must be or
+            return POr(self[0].to_drs(), self[2].to_drs())
 
 
-class CondChoice(List):
-    grammar = '(', PosInt, ',', [NegExpr, RelExpr, BinaryExpr], ')'
+class PCondChoice(List):
+    grammar = '(', PosInt, ',', [PNegExpr, PRelExpr, PBinaryExpr], ')'
+
     def to_drs(self):
         return PCond(int(self[0]), self[1].to_drs())
 
 
-class CondDecl(List):
-    grammar = optional(csl(CondChoice))
+class PCondDecl(List):
+    grammar = optional(csl(PCondChoice))
+
     def to_drs(self):
         return [x.to_drs() for x in self]
 
 
-class RefDecl(List):
+class PRefDecl(List):
     grammar = optional(csl(ProjRef))
+
     def to_drs(self):
         return [x.to_drs() for x in self]
 
 
 class MapDecl(List):
     grammar = optional(csl(Map))
+
     def to_drs(self):
         return [x.to_drs() for x in self]
 
 
-PdrsDecl.grammar = '<', PosInt, ',', '{', RefDecl, '}', ',', '{', CondDecl, '}', ',', '{', MapDecl, '}', '>'
-
+PdrsDecl.grammar = '<', PosInt, ',', '{', PRefDecl, '}', ',', '{', PCondDecl, '}', ',', '{', MapDecl, '}', '>'
 
 def parse_pdrs(s):
-    """Convert linear notation into a PDRS.
+    """Convert linear notation into a DRS. All whitespace, including new lines
+    are ignored. The parser uses pypeg2 which is written in python 3, therefore
+    the input string must be unicode.
+
+    The following names can be used for different operators (these are all
+    case insensitive):
+        - Negation operators: !, not, neg
+        - Implication operators (infix): imp, ->, =>, then
+        - Disjunction operators (infix): v, or
+        - Box operators: b, box, necessary
+        - Diamond operators: d, diamond, maybe.
+
+    The following rules apply:
+        - A PDRS is contained between '<' and '>'
+        - A list is contained between '{' and '}'. List's can have [0,inf] elements
+        - A tuple is contained between '(' and ')'. Tuples have fixed cardinality of 2.
+        - Elements are separated by a comma.
+        - An element can be: a PDRS, a list, a tuple, or an expression using the operators
+          described above.
+
+    For example, "The man is not happy" can be represented by
+        "<1,{},{ (1,not <5,{(2,x)},{(2,man(x)),(5,happy(x))},{(5,2)}>) },{}>"
+
+    Args:
+        s: The unicode string to parse.
+
+    Returns:
+        A PDRS instance.
 
     See Also:
+        parse_drs()
         marbles.ie.common.Showable.show()
     """
     # Remove all spaces in a string
@@ -115,3 +170,112 @@ def parse_pdrs(s):
     pt = p.parse(s, PdrsDecl)
     drs = pt[1].to_drs()
     return drs
+
+###########################################################################
+# DRS Parser
+
+
+class RelExpr(List):
+    grammar = Predicate, '(', csl(Predicate), ')'
+
+    def to_drs(self):
+        refs = [DRSRef(r) for r in self[1].encode('utf-8')]
+        return Rel(DRSRelation(self[0].encode('utf-8')), refs)
+
+
+class DrsDecl(List):
+    # Grammar is recursive so must declare with None
+    grammar = None
+
+    def to_drs(self):
+        return DRS(self[0].to_drs(), self[1].to_drs())
+
+
+class NegExpr(List):
+    grammar = NegateOp, DrsDecl
+
+    def to_drs(self):
+        return Neg(self[1].to_drs())
+
+
+class BinaryExpr(List):
+    grammar = DrsDecl, BinaryOp, DrsDecl
+
+    def to_drs(self):
+        if self[1] in ['d', 'diamond', 'maybe']:
+            return Diamond(self[0].to_drs(), self[2].to_drs())
+        elif self[1] in ['b', 'box', 'necessary']:
+            return Box(self[0].to_drs(), self[2].to_drs())
+        elif self[1] in ['imp', '=>', '->']:
+            return Imp(self[0].to_drs(), self[2].to_drs())
+        else:  # Must be or
+            return Or(self[0].to_drs(), self[2].to_drs())
+
+
+class CondChoice(List):
+    grammar = [NegExpr, RelExpr, BinaryExpr]
+
+    def to_drs(self):
+        return self[0].to_drs()
+
+
+class CondDecl(List):
+    grammar = optional(csl(CondChoice))
+
+    def to_drs(self):
+        return [x.to_drs() for x in self]
+
+
+class RefDecl(List):
+    grammar = optional(csl(Predicate))
+
+    def to_drs(self):
+        return [DRSRef(x.encode('utf-8')) for x in self]
+
+
+DrsDecl.grammar = '<', '{', RefDecl, '}', ',', '{', CondDecl, '}', '>'
+
+def parse_drs(s):
+    """Convert linear notation into a DRS. All whitespace, including new lines
+    are ignored. The parser uses pypeg2 which is written in python 3, therefore
+    the input string must be unicode.
+
+    The following names can be used for different operators (these are all
+    case insensitive):
+        - Negation operators: !, not, neg
+        - Implication operators (infix): imp, ->, =>, then
+        - Disjunction operators (infix): v, or
+        - Box operators: b, box, necessary
+        - Diamond operators: d, diamond, maybe.
+
+    The following rules apply:
+        - A DRS is contained between '<' and '>'
+        - A list is contained between '{' and '}'. List can have [0,inf] elements
+        - A tuple is contained between '(' and ')'. Tuples have fixed cardinality.
+        - Elements are separated by a comma.
+        - An element can be: a DRS, a list, a tuple, or an expression using the operators
+          described above.
+
+    For example, "The man is not happy" can be represented by
+        "<{x},{man(x), not <{},{happy(x)}>}>"
+
+    Args:
+        s: The unicode string to parse.
+
+    Returns:
+        A DRS instance.
+
+    See Also:
+        parse_pdrs()
+        marbles.ie.common.Showable.show()
+    """
+    # Remove all spaces in a string
+    p = Parser()
+    if isinstance(s, str):
+        s = s.decode('utf-8')
+    pt = p.parse(s, DrsDecl)
+    drs = pt[1].to_drs()
+    return drs
+
+
+
--- src/python/marbles/ie/pdrt/test/parser_test.py
+++ src/python/marbles/ie/pdrt/test/parser_test.py
@@ -1,12 +1,29 @@
 import unittest
 from ..pdrs import *
-from ..parse import parse_pdrs
+from ..parse import parse_pdrs, parse_drs
+from ..drs import DRSRelation
+from ..pdrs import *
+from ..drs import *
 
 
 class ParserTest(unittest.TestCase):
 
-    def test0_PDRTHaskellFormat(self):
-        #   1    2    3    4           5
+    def test0_PDRSHaskellFormat(self):
         s = u"<1,{},{ (1,not <5,{(2,x)},{(2,man(x)),(5,happy(x))}, {(5,2)}>) }, {}>"
-        parse_pdrs(s)
-        pass
+        p = parse_pdrs(s)
+        self.assertIsNotNone(p)
+        x = PDRS(1, [], [],
+                    [PCond(1, PNeg(PDRS(5, [MAP(5, 2)], [PRef(2, PDRSRef('x'))],
+                        [PCond(2, PRel(DRSRelation('man'),[PDRSRef('x')]))
+                        ,PCond(5, PRel(DRSRelation('happy'),[PDRSRef('x')]))])))])
+        self.assertEquals(x, p)
+
+    def test1_DRSHaskellFormat(self):
+        s = u"<{x},{man(x), not <{},{happy(x)}>}>"
+        p = parse_drs(s)
+        self.assertIsNotNone(p)
+        x = DRS([DRSRef('x')],
+                [Rel(DRSRelation('man'),[DRSRef('x')]),
+                 Neg(DRS([],[Rel(DRSRelation('happy'),[DRSRef('x')])]))])
+        self.assertEquals(x, p)
+
