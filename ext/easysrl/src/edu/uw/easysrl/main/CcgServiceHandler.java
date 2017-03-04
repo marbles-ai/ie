494785065d1756fb7b16d87076b4adb56027f43e
Paul Glendenning <paul@subjectlens.com>
2017-03-04 15:20:25 -0800

    Add naive session handler to support different output formats for each session.

--- ext/easysrl/src/edu/uw/easysrl/main/CcgServiceHandler.java
+++ ext/easysrl/src/edu/uw/easysrl/main/CcgServiceHandler.java
@@ -6,6 +6,8 @@ import java.util.*;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+import edu.uw.easysrl.semantics.lexicon.CompositeLexicon;
+import edu.uw.easysrl.semantics.lexicon.Lexicon;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -32,11 +34,6 @@ import edu.uw.easysrl.util.Util;
  */
 public class CcgServiceHandler extends LucidaServiceGrpc.LucidaServiceImplBase {
 	private static final Logger logger = LoggerFactory.getLogger(CcgServiceHandler.class);
-
-	/**
-	 * Since EasySRL is not thread-safe, we enforce the use to be single-threaded.
-	 */
-	private Lock infer_lock;
 
 	public class ConfigOptions implements EasySRL.CommandLineArguments {
 		private String modelPath;
@@ -84,87 +81,168 @@ public class CcgServiceHandler extends LucidaServiceGrpc.LucidaServiceImplBase {
 		public int getPort() { return 8084; }
 	}
 
-	private EasySRL.CommandLineArguments commandLineOptions;
-	private SRLParser parser;
-	private InputReader reader;
-	private ParsePrinter printer;
+	public interface Session {
+		SRLParser getParser();
+		InputReader getReader();
+		ParsePrinter getPrinter();
+		void lock();
+		void unlock();
+	}
+
+	public class SynchronizedSession implements Session  {
+		private SRLParser parser_;
+		private InputReader reader_;
+		private ParsePrinter printer_;
+		private Lock inferLock_;
+
+		public SRLParser getParser() { return parser_; }
+		public InputReader getReader() { return reader_; }
+		public ParsePrinter getPrinter() { return printer_; }
+		public void lock() {
+			inferLock_.lock();
+		}
+		public void unlock() {
+			inferLock_.unlock();
+		}
+
+		public SynchronizedSession(SRLParser parser, InputReader reader, ParsePrinter printer) {
+			this.parser_ = parser;
+			this.reader_  = reader;
+			this.printer_ = printer;
+			this.inferLock_ = new ReentrantLock();
+		}
+	}
+
+	private HashMap<String, Session> sessionCache_ = new HashMap();
+	private EasySRL.CommandLineArguments commandLineOptions_;
+	private Session defaultSession_;
 
 	/** Constructs the handler and initializes its EasySRL
 	 * object.
 	 */
 	public CcgServiceHandler(String pathToModel) {
-		commandLineOptions = new ConfigOptions(pathToModel);
-		infer_lock = new ReentrantLock();
+		commandLineOptions_ = new ConfigOptions(pathToModel);
 	}
 
 	public CcgServiceHandler(EasySRL.CommandLineArguments cmdLine) {
-		commandLineOptions = cmdLine;
-		infer_lock = new ReentrantLock();
+		commandLineOptions_ = cmdLine;
 	}
 
 	public void init() throws IOException, InterruptedException {
+		// No need to lock because this is called before gRPC service creation.
+		defaultSession_ = createSession("CCGBANK");
+	}
 
-		final EasySRL.InputFormat input = EasySRL.InputFormat.valueOf(commandLineOptions.getInputFormat().toUpperCase());
-		final File modelFolder = Util.getFile(EasySRL.absolutePath(commandLineOptions.getModel()));
+	private Session createSession(String oformat) throws IOException, InterruptedException {
+
+		final EasySRL.InputFormat input = EasySRL.InputFormat.valueOf(commandLineOptions_.getInputFormat().toUpperCase());
+		final File modelFolder = Util.getFile(EasySRL.absolutePath(commandLineOptions_.getModel()));
 
 		if (!modelFolder.exists()) {
-			logger.error("Couldn't load model from {}", commandLineOptions.getModel());
-			throw new InputMismatchException("Couldn't load model from from: " + commandLineOptions.getModel());
+			logger.error("Couldn't load model from {}", commandLineOptions_.getModel());
+			throw new InputMismatchException("Couldn't load model from from: " + commandLineOptions_.getModel());
 		}
 
 		final File pipelineFolder = new File(modelFolder, "/pipeline");
-		logger.debug("Loading model from {} ...", commandLineOptions.getModel());
-		final EasySRL.OutputFormat outputFormat = EasySRL.OutputFormat.valueOf("CCGBANK");
-		this.printer = outputFormat.printer;
-
+		logger.debug("Loading model from {} ...", commandLineOptions_.getModel());
+		final EasySRL.OutputFormat outputFormat = EasySRL.OutputFormat.valueOf(oformat);
+		ParsePrinter printer = outputFormat.printer;
 		SRLParser parser2;
+
 		if (pipelineFolder.exists()) {
 			// Joint model
 			final POSTagger posTagger = POSTagger.getStanfordTagger(new File(pipelineFolder, "posTagger"));
-			final PipelineSRLParser pipeline = EasySRL.makePipelineParser(pipelineFolder, commandLineOptions, 0.000001,
+			final PipelineSRLParser pipeline = EasySRL.makePipelineParser(pipelineFolder, commandLineOptions_, 0.000001,
 					printer.outputsDependencies());
-			parser2 = new BackoffSRLParser(new JointSRLParser(EasySRL.getParserBuilder(commandLineOptions).build(),
+			parser2 = new BackoffSRLParser(new JointSRLParser(EasySRL.getParserBuilder(commandLineOptions_).build(),
 					posTagger), pipeline);
 		} else {
 			// Pipeline
-			parser2 = EasySRL.makePipelineParser(modelFolder, commandLineOptions, 0.000001, printer.outputsDependencies());
+			parser2 = EasySRL.makePipelineParser(modelFolder, commandLineOptions_, 0.000001, printer.outputsDependencies());
 		}
 
-		this.reader = InputReader.make(EasySRL.InputFormat.valueOf(commandLineOptions.getInputFormat().toUpperCase()));
+		final SRLParser parser;
+		if (printer.outputsLogic()) {
+			// If we're outputing logic, load a lexicon
+			final File lexiconFile = new File(modelFolder, "lexicon");
+			final Lexicon lexicon = lexiconFile.exists() ? CompositeLexicon.makeDefault(lexiconFile)
+					: CompositeLexicon.makeDefault();
+			parser2 = new SRLParser.SemanticParser(parser2, lexicon);
+		}
+
+		InputReader reader;
+		if (oformat == "EXTENDED" || oformat == "PROLOG")
+			reader = InputReader.make(EasySRL.InputFormat.valueOf("POSandNERtagged".toUpperCase()));
+		else
+			reader = InputReader.make(EasySRL.InputFormat.valueOf(commandLineOptions_.getInputFormat().toUpperCase()));
+
+		/*
 		if ((outputFormat == EasySRL.OutputFormat.PROLOG || outputFormat == EasySRL.OutputFormat.EXTENDED)
 				&& input != EasySRL.InputFormat.POSANDNERTAGGED) {
 			String msg = "Must use \"-i POSandNERtagged\" for this output";
 			logger.error(msg);
 			throw new Error(msg);
 		}
+		*/
 		logger.debug("Model loaded: gRPC parser ready");
 
-		infer_lock.lock(); // limit concurrency because EasySRL is not thread-safe
-		this.parser = parser2;
-		infer_lock.unlock();
+		return new SynchronizedSession(parser2, reader, printer);
 	}
 
-	public String parse(String sentence) {
-		if (parser != null) {
-			List<CCGandSRLparse> parses = parser.parseTokens(reader.readInput(sentence)
+	public static String parse(Session session, String sentence) {
+		if (session.getParser() != null) {
+			List<CCGandSRLparse> parses = session.getParser().parseTokens(session.getReader().readInput(sentence)
 					.getInputWords());
-			return printer.printJointParses(parses, -1);
+			return session.getPrinter().printJointParses(parses,0);
 		}
 		return "";
+	}
+
+	synchronized private Session getSessionFromId(String sessionId, String outputFormat) throws IOException, InterruptedException {
+		if (outputFormat != null && sessionId != null && !sessionId.isEmpty()) {
+			Session session = sessionCache_.get(sessionId);
+			if (session == null) {
+				session = createSession(outputFormat);
+				sessionCache_.put(sessionId, session);
+			}
+			return session;
+		} else {
+			return sessionCache_.getOrDefault(sessionId, defaultSession_);
+		}
+	}
+
+	private Session getSessionFromId(String sessionId) throws IOException, InterruptedException {
+		return getSessionFromId(sessionId, null);
 	}
 
 	@Override
 	/** {@inheritDoc} */
 	public void create(Request request, StreamObserver<Empty> responseObserver) {
-		logger.debug(">> CREATE; User: " + request.getLUCID());
-		responseObserver.onNext(Empty.newBuilder().build());
-		responseObserver.onCompleted();
+		logger.debug("Create({}}", request.getLUCID());
+
+		if (request.getSpec().getContentList().isEmpty() ||
+				request.getSpec().getContentList().get(0).getDataList().isEmpty()) {
+			logger.info("empty content passed to service");
+			throw new IllegalArgumentException();
+		}
+
+		try {
+			responseObserver.onNext(Empty.newBuilder().build());
+			QueryInput queryInput = request.getSpec().getContentList().get(0);
+			getSessionFromId(request.getLUCID(), queryInput.getData(0).toString("UTF-8").toUpperCase());
+			responseObserver.onCompleted();
+		} catch (Exception e) {
+			logger.error("Create({}) failed to create session - {}", request.getLUCID(), e.getMessage());
+			//e.printStackTrace();
+			responseObserver.onError(e);
+		}
+
 	}
 
 	@Override
 	/** {@inheritDoc} */
 	public void learn(Request request, StreamObserver<Empty> responseObserver) {
-		logger.debug(">> LEARN; User: " + request.getLUCID());
+		logger.debug("Learn({}}", request.getLUCID());
 
 		responseObserver.onNext(Empty.newBuilder().build());
 		responseObserver.onCompleted();
@@ -173,7 +251,7 @@ public class CcgServiceHandler extends LucidaServiceGrpc.LucidaServiceImplBase {
 	@Override
 	/** {@inheritDoc} */
     public void infer(Request request, StreamObserver<Response> responseObserver) {
-		logger.debug(">> INFER; User: " + request.getLUCID());
+		logger.debug("Infer({}}", request.getLUCID());
 		
 	    if (request.getSpec().getContentList().isEmpty() || 
                 request.getSpec().getContentList().get(0).getDataList().isEmpty()) {
@@ -181,10 +259,11 @@ public class CcgServiceHandler extends LucidaServiceGrpc.LucidaServiceImplBase {
 	        throw new IllegalArgumentException();
 	    }
 
+	    Session session = null;
 		try {
 			QueryInput queryInput = request.getSpec().getContentList().get(0);
-
-			infer_lock.lock(); // limit concurrency because EasySRL is not thread-safe
+			session = getSessionFromId(request.getLUCID());
+			session.lock();
 
 			// FIXME: Response.msg should be an array
 			// Only look for the first item in content and data.
@@ -192,7 +271,7 @@ public class CcgServiceHandler extends LucidaServiceGrpc.LucidaServiceImplBase {
 			final StringBuilder output = new StringBuilder();
 			for (int i = 0; i < queryInput.getDataList().size(); ++i) {
 				String sentence = queryInput.getDataList().get(i).toString("UTF-8");
-				output.append(parse(sentence));
+				output.append(parse(session, sentence));
 				output.append("\n");
 			}
 
@@ -208,7 +287,8 @@ public class CcgServiceHandler extends LucidaServiceGrpc.LucidaServiceImplBase {
             logger.info("exception caught - {}", e.getMessage());
             responseObserver.onError(e);
 		} finally {
-			infer_lock.unlock(); // always unlock at the end
+			if (session != null)
+				session.unlock();
 		}
 	}
 }
