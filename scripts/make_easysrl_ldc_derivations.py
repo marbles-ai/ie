86daf0c0b6938c085e92cc26a389a73d018cd1eb
Paul Glendenning <paul@subjectlens.com>
2017-04-01 13:37:13 -0700

    Start EasySRL service if its not running. Use marbles.ie.grpc module for service access.

--- scripts/make_easysrl_ldc_derivations.py
+++ scripts/make_easysrl_ldc_derivations.py
@@ -3,14 +3,15 @@
 import os
 import re
 import sys
-import imp
-import grpc
+import time
+from subprocess import call
 
 # Modify python path
 projdir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
 pypath = os.path.join(projdir, 'src', 'python')
 datapath = os.path.join(pypath, 'marbles', 'ie', 'drt')
 sys.path.insert(0, pypath)
+from marbles.ie import grpc
 
 
 def die(s):
@@ -18,78 +19,80 @@ def die(s):
     sys.exit(1)
 
 
-grpcSrvc = os.path.join(projdir, 'build', 'grpc', 'python', 'marbles_service_pb2.py')
-sessionPrefix, _ = os.path.splitext(os.path.basename(__file__))
-if not os.path.exists(grpcSrvc):
-    die('gRPC service "%s" does not exist, run gradle build' % grpcSrvc)
-marbles_svc = imp.load_source("lucida_service", grpcSrvc)
-
-
-def create_query_input(typename, data):
-    query_input = marbles_svc.QueryInput()
-    query_input.type = typename
-    query_input.data.append(str(data))
-    return query_input
-
-
-def get_client_transport(host, port):
-    channel = grpc.insecure_channel('%s:%u' % (host, port))
-    stub = marbles_svc.LucidaServiceStub(channel)
-    return stub, channel
-
-
-def ccg_parse(client, sentence, sessionId):
-    query_input = create_query_input('text', sentence)
-    request = marbles_svc.Request()
-    request.LUCID = sessionId
-    request.spec.name = 'infer'
-    request.spec.content.extend([query_input])
-    response = client.infer(request)
-    return response.msg
+def print_progress(progress, tick=1, done=False):
+    progress += 1
+    if (progress / tick) >= 79 or done:
+        sys.stdout.write('.\n')
+        sys.stdout.flush()
+        return 0
+    elif (progress % tick) == 0:
+        sys.stdout.write('.')
+        sys.stdout.flush()
+    return progress
 
 
 idsrch = re.compile(r'[^.]+\.(?P<id>\d+)\.raw')
 
 if __name__ == '__main__':
     allfiles = []
-    esrlpath = os.path.join(projdir, 'data', 'ldc', 'easysrl')
+    esrlpath = os.path.join(projdir, 'data', 'ldc', 'easysrl', 'ccgbank')
     if not os.path.exists(esrlpath):
         os.mkdir(esrlpath)
 
-    sessionId = 'default'
-    stub, _ = get_client_transport('localhost', 8084)
+    svc_cmd = None
+    progress = 0
+    try:
+        # Check if service has started. If not start it.
+        stub, _ = grpc.get_client_transport('localhost', grpc.EASYSRL_PORT)
+        ccg = grpc.ccg_parse(stub, '')
+    except Exception:
+        # Not started
+        call([os.path.join(projdir, 'scripts', 'start_server.sh'), 'easysrl'])
+        time.sleep(4)   # Give it some time to lock session access
+        stub, _ = grpc.get_client_transport('localhost', grpc.EASYSRL_PORT)
+        # Call asynchronously - will wait until default session is created
+        ccg = grpc.ccg_parse(stub, '', timeout=120)
+        svc_cmd = os.path.join(projdir, 'scripts', 'stop_server.sh')
 
     failed_total = 0
     ldcpath = os.path.join(projdir, 'data', 'ldc', 'ccgbank_1_1', 'data', 'RAW')
     dirlist = os.listdir(ldcpath)
 
-    for fname in dirlist:
-        ldcpath1 = os.path.join(ldcpath, fname)
-        with open(ldcpath1, 'r') as fd:
-            lines = fd.readlines()
+    try:
+        for fname in dirlist:
+            ldcpath1 = os.path.join(ldcpath, fname)
+            with open(ldcpath1, 'r') as fd:
+                lines = fd.readlines()
 
-        m = idsrch.match(os.path.basename(ldcpath1))
-        if m is None:
-            continue
+            m = idsrch.match(os.path.basename(ldcpath1))
+            if m is None:
+                continue
 
-        derivations = []
-        failed_parse = []
-        for ln in lines:
-            # Parse with EasySRL via gRPC
-            try:
-                ccg = ccg_parse(stub, ln, sessionId)
-                derivations.append(ccg.replace('\n', ''))
-            except Exception as e:
-                failed_parse.append(ln.strip())
-        id = m.group('id')
-        if len(derivations) != 0:
-            with open(os.path.join(esrlpath, 'ccg_derivation%s.txt' % id), 'w') as fd:
-                fd.write('\n'.join(derivations))
+            derivations = []
+            failed_parse = []
+            for ln in lines:
+                # Parse with EasySRL via gRPC
+                try:
+                    ccg = grpc.ccg_parse(stub, ln)
+                    derivations.append(ccg.replace('\n', ''))
+                except Exception as e:
+                    failed_parse.append(ln.strip())
+                progress = print_progress(progress, 10)
+            id = m.group('id')
+            if len(derivations) != 0:
+                with open(os.path.join(esrlpath, 'ccg_derivation%s.txt' % id), 'w') as fd:
+                    fd.write('\n'.join(derivations))
 
-        failed_total += len(failed_parse)
-        if len(failed_parse) != 0:
-            with open(os.path.join(esrlpath, 'ccg_failed%s.txt' % id), 'w') as fd:
-                fd.write('\n'.join(failed_parse))
+            failed_total += len(failed_parse)
+            if len(failed_parse) != 0:
+                with open(os.path.join(esrlpath, 'ccg_failed%s.txt' % id), 'w') as fd:
+                    fd.write('\n'.join(failed_parse))
+    finally:
+        progress = print_progress(progress, 10, done=True)
+        if svc_cmd is not None:
+            # Stop service
+            stub = None
+            call([svc_cmd, 'easysrl'])
 
     if failed_total != 0:
         print('THERE WERE %d PARSE FAILURES' % failed_total)
