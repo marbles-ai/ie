66b6c95cb862ca824bd83b5127c75f35e6931552
Paul Glendenning <paul@subjectlens.com>
2017-05-28 20:09:50 -0700

    Add devel check.

--- src/python/marbles/__init__.py
+++ src/python/marbles/__init__.py
@@ -1,2 +1,4 @@
+import os
 
-
+_projdir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+USE_DEVEL_PATH = os.path.exists(os.path.join(_projdir, 'src', 'python', 'marbles'))
