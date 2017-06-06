4fd4a4a1ee2991b96dc198ce745b6784e42f75f1
Paul Glendenning <paul@subjectlens.com>
2017-06-06 12:08:58 -0700

    Clear error cache regularly.

--- src/python/marbles/log.py
+++ src/python/marbles/log.py
@@ -2,16 +2,25 @@ from __future__ import unicode_literals, print_function
 import logging
 import inspect
 import time
+from marbles import Properties
 
 
 class ExceptionRateLimitedLogAdaptor(logging.LoggerAdapter, object):
     '''Rate limit exception log adaptor.'''
 
-    def __init__(self, logger, rlimit=0.0):
+    def __init__(self, logger, rlimit=None):
+        """Constructor.
+
+        Args:
+            logger: A logger instance.
+            rlimit: The rate limit value. Zero disables. If None then the default of marbles.Properties.exception_rlimit
+                is used.
+        """
         super(self.__class__, self).__init__(logger, {})
         self.error_cache = {}
         self.logger = logger
-        self.rlimit = rlimit
+        self.rlimit = rlimit or Properties.exception_rlimit
+        self.last_update_time = time.time()
 
     def find_caller(self):
         frmrec = inspect.stack()[2]
@@ -28,6 +37,12 @@ class ExceptionRateLimitedLogAdaptor(logging.LoggerAdapter, object):
                 callerid = "%s:%s:%d" % (type(exc_info[0][1]).__name__, caller[0], caller[1])
 
             tmnew = time.time()
+
+            # Regularly clear cache to recover memory
+            if (tmnew - self.last_update_time) > (2*self.rlimit):
+                self.error_cache = {}
+            self.last_update_time = tmnew
+
             if callerid in self.error_cache:
                 tmold = self.error_cache[callerid]
                 self.error_cache[callerid] = tmnew
