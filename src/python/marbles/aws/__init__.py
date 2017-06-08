2a7729ab2495f06871c2798b5542e6c6d4a67730
Paul Glendenning <paul@subjectlens.com>
2017-06-07 23:30:24 -0700

    Move AWS Service classes into this module.

---
+++ src/python/marbles/aws/__init__.py
@@ -0,0 +1,340 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, print_function
+import boto3
+import requests
+import StringIO
+import base64
+import json
+import time
+import logging
+from nltk.tokenize import sent_tokenize
+from marbles.ie import grpc
+from marbles.ie.compose import CO_ADD_STATE_PREDICATES, CO_NO_VERBNET, CO_BUILD_STATES
+from marbles.ie.ccg import parse_ccg_derivation2 as parse_ccg_derivation
+from marbles.ie.ccg2drs import process_ccg_pt
+from marbles.log import ExceptionRateLimitedLogAdaptor
+_logger = ExceptionRateLimitedLogAdaptor(logging.getLogger(__name__))
+#python -m nltk.downloader punkt
+
+
+class ServiceState(object):
+    def __init__(self, logger=None):
+        self.terminate = False
+        self.pass_on_exceptions = False
+        if logger is not None and not isinstance(logger, ExceptionRateLimitedLogAdaptor):
+            self.logger = ExceptionRateLimitedLogAdaptor(logger)
+        else:
+            self.logger = logger
+
+    def wait(self, seconds):
+        time.sleep(seconds)
+
+    def time(self):
+        return time.time()
+
+
+def receive_messages(*args, **kwargs):
+    queue = args[0]
+    args = args[1:]
+    if queue is not None:
+        maxmsgs = filter(lambda x: x[0] == 'MaxNumberOfMessages', kwargs.iteritems())
+        if len(maxmsgs) != 0:
+            for msg in queue.receive_messages(*args, **kwargs):
+                yield msg
+        else:
+            msgs = [ None ]
+            while len(msgs) != 0:
+                msgs = queue.receive_messages(*args, **kwargs)
+                for m in msgs:
+                    yield m
+
+
+class AwsNewsQueueBase(object):
+    """News queue base class."""
+
+    def __init__(self):
+        self.s3 = boto3.resource('s3')
+        self.sqs = None
+        self.news_queue = None
+
+
+class AwsNewsQueueWriterResources(AwsNewsQueueBase):
+    """AWS news queue writer resources."""
+
+    def __init__(self, queue_name=None):
+        super(AwsNewsQueueWriterResources, self).__init__()
+        if queue_name:
+            self.sqs = boto3.resource('sqs')
+            if queue_name == 'default-queue':
+                resp = self.sqs.get_queue_by_name(QueueName='marbles-ai-rss-aggregator')
+            else:
+                resp = self.sqs.get_queue_by_name(QueueName=queue_name)
+            self.news_queue = resp
+        else:
+            self.sqs = None
+            self.news_queue = None
+
+
+class AwsNewsQueueReaderResources(AwsNewsQueueBase):
+    """AWS news queue reader resources - includes gRPC client."""
+
+    def __init__(self, stub, news_queue_name, ccg_queue_name=None):
+        super(AwsNewsQueueReaderResources, self).__init__()
+        self.sqs = boto3.resource('sqs')
+        self.stub = stub
+        if news_queue_name == 'default-queue':
+            resp = self.sqs.get_queue_by_name(QueueName='marbles-ai-rss-aggregator')
+        else:
+            resp = self.sqs.get_queue_by_name(QueueName=news_queue_name)
+        self.news_queue = resp
+
+        if ccg_queue_name:
+            if ccg_queue_name == 'default-queue':
+                resp = self.sqs.get_queue_by_name(QueueName='marbles-ai-discourse-logic')
+            else:
+                resp = self.sqs.get_queue_by_name(QueueName=ccg_queue_name)
+            self.ccg_queue = resp
+        else:
+            self.ccg_queue = None
+
+
+class AwsNewsQueueWriter(object):
+    """RSS/Atom reader"""
+
+    def __init__(self, aws, state, sources, browser):
+        self.browser = browser
+        self.aws = aws
+        self.sources = sources
+        self.bucket_cache = set()
+        self.hash_cache = {}
+        self.state = state
+
+    @property
+    def logger(self):
+        global _logger
+        return self.state.logger if self.state.logger is not None else _logger
+
+    def close(self):
+        self.browser.close()
+
+    def check_bucket_exists(self, bucket):
+        if bucket in self.bucket_cache:
+            return True
+        # Bucket count never exceeds a few 100
+        # Refresh all so we handle deletions
+        self.bucket_cache = set()
+        for bkt in self.aws.s3.buckets.all():
+            self.bucket_cache.add(bkt.name)
+        return bucket in self.bucket_cache
+
+    def check_hash_exists(self, hash):
+        if hash in self.hash_cache:
+            return True
+        exists = 0 != len([x for x in self.aws.s3.Bucket('marbles-ai-feeds-hash').objects.filter(Prefix=hash)])
+        if exists:
+            self.hash_cache[hash] = self.state.time()
+        return exists
+
+    def clear_bucket_cache(self):
+        self.bucket_cache = set()
+
+    def retire_hash_cache(self, limit):
+        """Retire least recently used in hash cache."""
+        if limit >= len(self.hash_cache):
+            return
+        # Each value is a timestamp floating point
+        vk_sort = sorted(self.hash_cache.items(), key=lambda x: x[1])
+        vk_sort.reverse()
+        self.hash_cache = dict(vk_sort[0:limit])
+
+    def refresh(self):
+        if self.state.terminate:
+            return
+        # TODO: Interleave work
+        for src in self.sources:
+            if self.state.terminate:
+                return
+            for rss in src.feeds:
+                if self.state.terminate:
+                    return
+                rss.refresh()
+                self.state.wait(1)
+
+    def read_all(self, ignore_read):
+        for src in self.sources:
+            self.read_from_source(src, ignore_read)
+            if self.state.terminate:
+                return
+        self.browser.get_blank()
+
+    def read_from_source(self, src, ignore_read):
+        if self.state.terminate:
+            return
+        articles = []
+        for rss in src.feeds:
+            articles.extend(rss.get_articles(ignore_read=ignore_read)) # Returns unread articles
+
+        errors = 0
+        for a in articles:
+            if self.state.terminate:
+                return
+            hash = ''
+            try:
+                arc = a.archive(src.scraper)
+                bucket, objname = a.get_aws_s3_names(arc['content'])
+                hash = objname.split('/')[-1]
+                self.logger.debug(hash + ' - ' + arc['title'])
+                if self.check_hash_exists(hash):
+                    continue
+
+                if not self.check_bucket_exists(bucket):
+                    # Create a bucket
+                    self.aws.s3.create_bucket(Bucket=bucket,
+                                              CreateBucketConfiguration={'LocationConstraint': 'us-west-1'})
+                    self.bucket_cache.add(bucket)
+
+                strm = StringIO.StringIO()
+                json.dump(arc, strm, indent=2)
+                objpath = objname+'.json'
+                data = strm.getvalue()
+                self.aws.s3.Object(bucket, objpath).put(Body=data)
+
+                # Add to hash listing - allows us to find entries by hash
+                self.aws.s3.Object('marbles-ai-feeds-hash', hash).put(Body=objpath)
+
+                # Add to AWS processing queue
+                if self.aws.news_queue:
+                    attributes = {
+                        's3': {
+                            'DataType': 'String',
+                            'StringValue': bucket + '/' + objpath
+                        },
+                        'hash': {
+                            'DataType': 'String',
+                            'StringValue': hash
+                        }
+                    }
+                    # Store thumbnail in message so website has immediate access
+                    d = arc.setdefault('media', {})
+                    if d.setdefault('thumbnail', -1) >= 0:
+                        try:
+                            media = d['content'][ d['thumbnail']]
+                            r = requests.get(media['url'])
+                            b64media = base64.b64encode(r.content)
+                            attributes['media_thumbnail'] = {
+                                'DataType': 'String',
+                                'StringValue': b64media
+                            }
+                            attributes['media_type'] = {
+                                'DataType': 'String',
+                                'StringValue': media['type']
+                            }
+                            arc['media']['thumbnail64'] = b64media
+                        except requests.ConnectionError as e:
+                            # Non critical error - can happen when replaying old stories
+                            self.logger.warning('Media not found - %s', str(e))
+
+                    # Send the message to our queue
+                    response = self.aws.news_queue.send_message(MessageAttributes=attributes,
+                                                                MessageBody=data)
+                    self.logger.debug('Sent s3(%s) -> msg(%s)', hash, response['MessageId'])
+
+                # Update hash cache
+                self.hash_cache[hash] = self.state.time()
+
+            except KeyboardInterrupt:
+                # Pass on so we can close when running in console mode
+                raise
+
+            except Exception as e:
+                # Exception source defined by file-name:line-number:exeption-class:hash
+                self.logger.exception('AwsNewsQueueWriter.read_from_source', exc_info=e, rlimitby=hash)
+                errors += 1
+                if self.state.pass_on_exceptions:
+                    raise
+
+            self.state.wait(1)
+
+
+class AwsNewsQueueReader(object):
+    """CCG Parser handler"""
+
+    def __init__(self, aws, state, options=0):
+        self.aws = aws
+        self.state = state
+        self.options = options
+
+    @property
+    def logger(self):
+        return self.state.logger
+
+    def run(self):
+        """Process messages."""
+
+        for message in receive_messages(self.aws.news_queue, MessageAttributeNames=['All']):
+            # Attributes will be passed onto next queue
+            attributes = message.message_attributes
+            body = json.loads(message.body)
+            retry = 3
+            ccgbank = None
+            title = body['title']
+            paragraphs_in = filter(lambda y: len(y) != 0, map(lambda x: x.strip(), body['content'].split('\n')))
+            paragraphs_out = []
+            for p in paragraphs_in:
+                sentences = filter(lambda x: len(x.strip()) != 0, sent_tokenize(p))
+                paragraphs_out.append(sentences)
+
+            if self.state.terminate:
+                break
+
+            result = {}
+            result['title'] = {}
+            while retry:
+                try:
+                    ccgbank = grpc.ccg_parse(self.aws.stub, title, grpc.DEFAULT_SESSION)
+                    pt = parse_ccg_derivation(ccgbank)
+                    ccg = process_ccg_pt(pt, options=self.options)
+                    result['title']['lexemes'] = [x.get_json() for x in ccg.get_span()]
+                    result['title']['constituents'] = [c.get_json() for c in ccg.constituents]
+                    ccgpara = []
+                    result['paragraphs'] = ccgpara
+                    for sentences in paragraphs_out:
+                        ccgsent = []
+                        ccgpara.append(ccgsent)
+                        for s in sentences:
+                            ccgbank = grpc.ccg_parse(self.aws.stub, s, grpc.DEFAULT_SESSION)
+                            pt = parse_ccg_derivation(ccgbank)
+                            ccg = process_ccg_pt(pt, options=self.options)
+                            ccgentry = {}
+                            ccgentry['lexemes'] = [x.get_json() for x in ccg.get_span()]
+                            ccgentry['constituents'] = [c.get_json() for c in ccg.constituents]
+                            ccgsent.append(ccgentry)
+                    break   # exit while
+                except requests.exceptions.ConnectionError as e:
+                    time.sleep(0.25)
+                    retry -= 1
+                    self.logger.exception('AwsNewsQueueReader.run', exc_info=e)
+                    if self.state.pass_on_exceptions:
+                        raise
+                except Exception as e:
+                    # After X reads AWS sends the item to the dead letter queue.
+                    # X is configurable in AWS console.
+                    retry = 0
+                    self.logger.exception('AwsNewsQueueReader.run', exc_info=e,
+                                          rlimitby=attributes['hash']['StringValue'])
+                    if self.state.pass_on_exceptions:
+                        raise
+
+                if self.state.terminate:
+                    break
+
+            # retry == 0 indicates failure
+            if retry == 0:
+                continue
+
+            # Let the queue know that the message is processed
+            message.delete()
+            if self.aws.news_queue:
+                response = self.aws.news_queue(MessageAttributes=attributes,
+                                               MessageBody=result)
---
+++ src/python/marbles/test/aws_test.py
@@ -0,0 +1,184 @@
+from __future__ import unicode_literals, print_function
+import unittest
+import os
+import json
+import logging
+from moto import mock_s3, mock_sqs
+from marbles import aws
+from marbles.ie import grpc
+from marbles.ie.compose import CO_NO_WIKI_SEARCH
+import boto3
+#from botocore.exceptions import QueueDoesNotExist
+
+
+datadir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'ie', 'test', 'data')
+datafiles = [
+    os.path.join(datadir, '77a7e0b715396ba02b7fe12aa6c87336.json'),
+    os.path.join(datadir, '774e08800dfe1aaa4598149cf5208df3.json'),
+    os.path.join(datadir, 'a223515d444768b863e3a4d94b2a19a3.json'),
+    os.path.join(datadir, 'c0053ac368cf2e5c2599f035f2ee4eea.json')
+]
+
+
+class MockedState(aws.ServiceState):
+    def __init__(self, *args, **kwargs):
+        super(MockedState, self).__init__(*args, **kwargs)
+        self.tick = 1.0
+        self.pass_on_exceptions = True
+
+    def wait(self, seconds):
+        self.tick += 1.0
+
+    def time(self):
+        self.tick += 1.0
+        return self.tick
+
+
+class MockedArticle(object):
+    def __init__(self, fn, resp):
+        self.resp = resp
+        hash, _ = os.path.splitext(os.path.basename(fn))
+        self.fn = '02/testsrc/this-is-a-test/' + hash
+
+    def archive(self, scraper):
+        return self.resp
+
+    def get_aws_s3_names(self, text):
+        return 'test-bucket-2017-06', self.fn
+
+
+# Mock rss feeds and src
+class MockedFeed(object):
+    def __init__(self):
+        # Mock response for Article.archive(scraper)
+        self.articles = []
+        for fn in datafiles:
+            with open(fn, 'r') as fp:
+                self.articles.append(MockedArticle(fn, json.load(fp)))
+
+    def get_articles(self, ignore_read):
+        return self.articles
+
+
+class MockedNewsSource(object):
+    def __init__(self):
+        self.feeds = [ MockedFeed() ]
+        self.scraper = None
+
+
+class MockedBrowser(object):
+    def get_blank(self):
+        pass
+
+    def close(self):
+        pass
+
+
+class TestAWS(unittest.TestCase):
+
+    def setUp(self):
+        self.mocked_src = MockedNewsSource()
+        logger = logging.getLogger(__name__)
+        logger.addHandler(logging.NullHandler())
+        self.state = MockedState(logger)
+
+    @mock_s3
+    @mock_sqs
+    def test1_NewsQueueWriter(self):
+        # Create AWS virtual resources
+        try:
+            awsres = aws.AwsNewsQueueWriterResources('default-queue')
+            self.fail()
+        except Exception as e:
+            pass
+        sqs = boto3.client('sqs')
+        response = sqs.create_queue(QueueName='marbles-ai-rss-aggregator')
+        self.assertTrue('QueueUrl' in response)
+        s3 = boto3.client('s3')
+        response = s3.create_bucket(Bucket='marbles-ai-feeds-hash')
+        # Boto3 documentation says 'Location' is in the response however when mocking boto3 through moto
+        # we get different keys. Not sure if this is error in moto or documentation.
+        #self.assertTrue('Location' in response)
+        self.assertTrue('ResponseMetadata' in response)
+        self.assertTrue('HTTPStatusCode' in response['ResponseMetadata'])
+        self.assertTrue(response['ResponseMetadata']['HTTPStatusCode'] == 200)
+
+        awsres = aws.AwsNewsQueueWriterResources('default-queue')
+        nqw = aws.AwsNewsQueueWriter(awsres, self.state, [self.mocked_src], MockedBrowser())
+        nqw.read_all(True)
+        # Check s3
+        actual = [bkt.name for bkt in awsres.s3.buckets.all()]
+        expected = ['test-bucket-2017-06', 'marbles-ai-feeds-hash']
+        self.assertListEqual(actual, expected)
+        self.assertEqual(len(nqw.hash_cache), 4)
+        self.assertTrue(nqw.check_hash_exists('77a7e0b715396ba02b7fe12aa6c87336'))
+        self.assertTrue(nqw.check_hash_exists('774e08800dfe1aaa4598149cf5208df3'))
+        self.assertTrue(nqw.check_hash_exists('a223515d444768b863e3a4d94b2a19a3'))
+        self.assertTrue(nqw.check_hash_exists('c0053ac368cf2e5c2599f035f2ee4eea'))
+
+        # Retiring pushes out oldest
+        nqw.retire_hash_cache(2)
+        self.assertEqual(len(nqw.hash_cache), 2)
+        self.assertTrue('a223515d444768b863e3a4d94b2a19a3' in nqw.hash_cache)
+        self.assertTrue('c0053ac368cf2e5c2599f035f2ee4eea' in nqw.hash_cache)
+
+        self.assertTrue(nqw.check_hash_exists('77a7e0b715396ba02b7fe12aa6c87336'))
+        self.assertEqual(len(nqw.hash_cache), 3)
+        self.assertTrue(nqw.check_hash_exists('774e08800dfe1aaa4598149cf5208df3'))
+        self.assertEqual(len(nqw.hash_cache), 4)
+
+        # Read message queue
+        hashes = set(nqw.hash_cache.iterkeys())
+        msgcount = 0
+
+        for message in aws.receive_messages(awsres.news_queue, MessageAttributeNames=['All']):
+            self.assertIsNotNone(message.message_attributes)
+            hash = message.message_attributes.get('hash').get('StringValue')
+            self.assertTrue(hash in hashes)
+            hashes.remove(hash)
+            message.delete()
+            msgcount += 1
+
+        self.assertEqual(msgcount, 4)
+
+
+    @mock_s3
+    @mock_sqs
+    def test2_NewsQueueReader(self):
+        # Create AWS virtual resources
+        sqs = boto3.client('sqs')
+        sqs.create_queue(QueueName='marbles-ai-rss-aggregator')
+        sqs.create_queue(QueueName='marbles-ai-discourse-logic')
+        s3 = boto3.client('s3')
+        s3.create_bucket(Bucket='marbles-ai-feeds-hash')
+
+        # Create marbles resources
+        svc = grpc.CcgParserService(daemon='easysrl', logger=self.state.logger)
+        stub = svc.open_client()
+        awsres = aws.AwsNewsQueueReaderResources(stub, 'default-queue', 'default-queue')
+
+        # Write to news_queue
+        nqw = aws.AwsNewsQueueWriter(awsres, self.state, [self.mocked_src], MockedBrowser())
+        nqw.read_all(True)
+
+        # Read from news_queue and write to ccg_queue
+        nqr = aws.AwsNewsQueueReader(awsres, self.state, CO_NO_WIKI_SEARCH)
+        nqr.run()
+
+        # Read ccg_queue
+        hashes = set(nqw.hash_cache.iterkeys())
+        self.assertEqual(len(hashes), 4)
+        msgcount = 0
+
+        for message in aws.receive_messages(awsres.ccg_queue, MessageAttributeNames=['All']):
+            self.assertIsNotNone(message.message_attributes)
+            hash = message.message_attributes.get('hash').get('StringValue')
+            self.assertTrue(hash in hashes)
+            hashes.remove(hash)
+            message.delete()
+            msgcount += 1
+
+        self.assertEqual(msgcount, 4)
+
+if __name__ == '__main__':
+    unittest.main()
