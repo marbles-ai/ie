6b673d72378f01b5684136e826ebf17c56b776bf
Tommy Tracy II <tjt7a@virginia.edu>
2017-06-28 21:28:32 -0400

    New dockerfile with nginx support; adding tree drawing stuff

--- site/app/main.py
+++ site/app/main.py
@@ -1,10 +1,11 @@
-from flask import render_template
-from flask import jsonify
-from app import app
+from flask import Flask, send_file, render_template, jsonify
+import os
 import logging
-#import wordcloud
+
+app = Flask(__name__)
 
 i = 0
+
 
 # For now, let's use this for examples
 def getGlobalCount():
@@ -12,10 +13,12 @@ def getGlobalCount():
     i = i + 1
     return i - 1
 
+
 # Generates a wordcloud background
 def generate_background():
     print "Generating a new background"
 #    wordcloud.generate_wordcloud()
+
 
 @app.route('/')
 @app.route('/index')
@@ -23,12 +26,24 @@ def index():
     return render_template('index.html',
                            title='Home')
 
+
+@app.route('/demo')
+def demo():
+    return render_template('demo.html',
+                           title='Demo')
+
+
 @app.route('/getData', methods= ['GET'])
 def stuff():
     i = getGlobalCount()
     return jsonify(article="Article: "+ str(i), content="Content: " + str(i))
 
+
 @app.route('/getBackground', methods= ['GET'])
 def background():
     background_name = generate_background()
     return jsonify(background=background_name)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True, port=80)
