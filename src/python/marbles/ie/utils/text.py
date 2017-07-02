3a948ff4725e438ebc27066587dd03f92728ed9e
Paul Glendenning <paul@subjectlens.com>
2017-07-01 23:35:51 -0700

    Handle abbrieviations such as hadn't, should've etc...

--- src/python/marbles/ie/utils/text.py
+++ src/python/marbles/ie/utils/text.py
@@ -6,6 +6,7 @@ import regex as re  # Has better support for unicode
 _UPUNCT = re.compile(r'([,:;\u00a1\u00a7\u00b6\u00b7\u00bf])', re.UNICODE)
 _UDQUOTE = re.compile(r'["\u2033\u2034\u2036\u2037\u201c\u201d]', re.UNICODE)
 _USQUOTE = re.compile(r"\u2032([^\u2032\u2035]+)\u2035", re.UNICODE)
+_UAPOS = re.compile(r"(?<=[a-z])(['](?:ll|s|ve|nt))(?=\s|$)", re.UNICODE | re.IGNORECASE)
 
 
 def preprocess_sentence(text):
@@ -21,6 +22,9 @@ def preprocess_sentence(text):
     text = _USQUOTE.sub(r" ' \1 ' ", text).replace('\u2019', "'")
     text = _UDQUOTE.sub(r' " ', text)
     text = _UPUNCT.sub(r' \1 ', text)
+    text = _UAPOS.sub(r' \1', text)
+    text = text.replace("wo n't", "won't")
+    text = text.replace("ca n't", "can't")
 
     # TODO: Fixup spelling errors
     return text
