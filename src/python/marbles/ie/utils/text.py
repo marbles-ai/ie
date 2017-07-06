11a15b3ebb90229ade3edc1f91eba189478b200a
Paul Glendenning <paul@marbles.ai>
2017-07-06 09:21:49 -0700

    Improve punctuation handling.

--- src/python/marbles/ie/utils/text.py
+++ src/python/marbles/ie/utils/text.py
@@ -6,7 +6,8 @@ import regex as re  # Has better support for unicode
 _UPUNCT = re.compile(r'([,:;\u00a1\u00a7\u00b6\u00b7\u00bf])', re.UNICODE)
 _UDQUOTE = re.compile(r'["\u2033\u2034\u2036\u2037\u201c\u201d]', re.UNICODE)
 _USQUOTE = re.compile(r"\u2032([^\u2032\u2035]+)\u2035", re.UNICODE)
-_UAPOS = re.compile(r"(?<=[a-z])(['](?:ll|s|ve|nt))(?=\s|$)", re.UNICODE | re.IGNORECASE)
+_USQL = re.compile(r"(?<=[a-z])(['](?:ll|s|ve|nt|m|re|d))(?=\s|$)", re.UNICODE | re.IGNORECASE)
+_USQR = re.compile(r"(['])(?!(?:ll|s|ve|nt|m|re|d)(?:\s|$))", re.UNICODE | re.IGNORECASE)
 
 
 def preprocess_sentence(text):
@@ -19,12 +20,15 @@ def preprocess_sentence(text):
         A sentence.
     """
     # Perform Unicode-Ascii substitutions and add spaces around punctuation
-    text = _USQUOTE.sub(r" ' \1 ' ", text).replace('\u2019', "'")
+    text = _USQUOTE.sub(r"'\1'", text).replace('\u2019', "'")
     text = _UDQUOTE.sub(r' " ', text)
     text = _UPUNCT.sub(r' \1 ', text)
-    text = _UAPOS.sub(r' \1', text)
+    text = _USQL.sub(r' \1', text)
+    text = _USQR.sub(r'\1 ', text)
+    # wa, ca, sha are not part of the vocab
     text = text.replace("wo n't", "won't")
     text = text.replace("ca n't", "can't")
+    text = text.replace("sha n't", "shan't")
 
     # TODO: Fixup spelling errors
     return text
