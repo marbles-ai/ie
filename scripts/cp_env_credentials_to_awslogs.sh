4102fa3314d10c37d403582b9734e1b49d45d6f2
Paul Glendenning <paul@marbles.ai>
2017-07-08 18:10:48 -0700

    Grab credentials from ~/.aws if they exist.

--- scripts/cp_env_credentials_to_awslogs.sh
+++ scripts/cp_env_credentials_to_awslogs.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
 
-[ "x$AWS_ACCESS_KEY_ID" != "x" ] || exit 1
-[ "x$AWS_SECRET_ACCESS_KEY" != "x" ] || exit 1
+HAVE_ENV_CREDENTIALS=1
+[ "x$AWS_ACCESS_KEY_ID" != "x" ] || HAVE_ENV_CREDENTIALS=0
+[ "x$AWS_SECRET_ACCESS_KEY" != "x" ] || HAVE_ENV_CREDENTIALS=0
 [ "x$AWS_DEFAULT_REGION" != "x" ] || AWS_DEFAULT_REGION="us-west-1"
 
-echo "[plugins]" > /var/awslogs/etc/aws.conf
-echo "cwlogs = cwlogs" >> /var/awslogs/etc/aws.conf
-echo "" >> /var/awslogs/etc/aws.conf
-echo "[default]" >> /var/awslogs/etc/aws.conf
-echo "region = $AWS_DEFAULT_REGION" >> /var/awslogs/etc/aws.conf
-echo "aws_access_key_id = $AWS_ACCESS_KEY_ID" >> /var/awslogs/etc/aws.conf
-echo "aws_secret_access_key = $AWS_SECRET_ACCESS_KEY" >> /var/awslogs/etc/aws.conf
+if [ $HAVE_ENV_CREDENTIALS -eq 1 ]; then
+	echo "[plugins]" > /var/awslogs/etc/aws.conf
+	echo "cwlogs = cwlogs" >> /var/awslogs/etc/aws.conf
+	echo "" >> /var/awslogs/etc/aws.conf
+	echo "[default]" >> /var/awslogs/etc/aws.conf
+	echo "region = $AWS_DEFAULT_REGION" >> /var/awslogs/etc/aws.conf
+	echo "aws_access_key_id = $AWS_ACCESS_KEY_ID" >> /var/awslogs/etc/aws.conf
+	echo "aws_secret_access_key = $AWS_SECRET_ACCESS_KEY" >> /var/awslogs/etc/aws.conf
+elif [ -e ~/.aws/credentials ]; then
+	echo "[plugins]" > /var/awslogs/etc/aws.conf
+	echo "cwlogs = cwlogs" >> /var/awslogs/etc/aws.conf
+	cat ~/.aws/credentials >> /var/awslogs/etc/aws.conf
+	echo "region = $AWS_DEFAULT_REGION" >> /var/awslogs/etc/aws.conf
+fi
