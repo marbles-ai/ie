872f2418fe167f3185b64982342eede0f6916b49
Paul Glendenning <paul@marbles.ai>
2017-07-08 22:41:28 -0700

    check all os versions exist

--- deps/phantomjs/check.sh
+++ deps/phantomjs/check.sh
@@ -1,2 +1,4 @@
 [ -e ../../src/python/marbles/newsfeed/data/phantomjs-linux ] || exit 1
+[ -e ../../src/python/marbles/newsfeed/data/phantomjs-ubuntu ] || exit 1
+[ -e ../../src/python/marbles/newsfeed/data/phantomjs-osx ] || exit 1
 exit 0
