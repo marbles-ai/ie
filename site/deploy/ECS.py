1b1dbba32bf3b5828757d57e7ef6b48530a66726
Tommy Tracy II <tjt7a@virginia.edu>
2017-07-12 01:47:55 -0400

    Still working on deploy script

--- site/deploy/ECS.py
+++ site/deploy/ECS.py
@@ -6,22 +6,100 @@ class ECS:
     def __init__(self):
         self.ecs = boto3.client('ecs')
 
+    # Describe passed clusters
+    def describe_cluster(self, clusters):
+        response = self.ecs.describe_clusters(
+            clusters = clusters
+        )
+        return response
+
+    def list_clusters(self):
+        # Do stuff
+
+    def describe_container_instances(self, cluster, containerInstance):
+        response = self.ecs.describe_container_instances(
+            cluster = cluster,
+            containerInstance = containerInstance
+        )
+        return response
+
     def create_cluster(self, name):
         response = self.ecs.create_cluster(clusterName=name)
         return response
 
+    # Can't delete a cluster without first deregistering the containers from it
     def delete_cluster(self, name):
         response = self.ecs.delete_cluster(cluster=name)
         return response
 
+    # Deregister an ECS container instance from a cluster; this cluster can't run tasks anymore
+    def deregister_container_instance(self, cluster, containerInstance):
+        response = self.ecs.register_container_instance(
+            cluster = cluster,
+            containerInstance = containerInstance
+        )
+
+
+    def list_services(self):
+        # Do stuff
+
+    def describe_services(self):
+        # do stuff
+
+    # Run & maintain a number of tasks
+    def create_service(self, cluster, serviceName, taskDefinition, loadBalancers, desiredCount, ct, role):
+        response = self.ecs.create_services(
+            cluster=cluster,
+            serviceName=serviceName,
+            taskDefinition=taskDefinition,
+            loadBalancers=loadBalancers,
+            desiredCount=desiredCount,
+            clientToken=ct,
+            role=role
+        )
+        return response
+
+    # Delete a service from within a cluster
+    def delete_service(self, cluster, service):
+        response = self.ecs.delete_service(
+            cluster = cluster,
+            service = service
+        )
+
+
+
 
 
     ################### TASKS ###################
+
+    def describe_task_definitions(self):
+        # Do stuff
+
+    def describe_tasks(self):
+        # Do sdtuff
+
+    def list_tasks(self):
+        # Do stuff
+
+    def list_task_definitions(self):
+        # Do stuff
 
     def start_task(self, cluster, definition, start_task, instance, group):
         response = self.ecs.start_task(cluster=cluster,
                                     taskDefinition=definition,
                                     containerInstances=instances,
                                     group=group)
-
         return response
+
+    def run_task(self):
+        # do stuff
+
+    def stop_task(self):
+        # do stuff
+
+    def deregister_task_definition(self, taskDefinition):
+        response = self.ecs.deregister_task_definition(
+            taskDefinition=taskDefinition
+        )
+        return response
+
--- site/deploy/deploy.py
+++ site/deploy/deploy.py
@@ -1,9 +1,35 @@
 from EC2 import *
 
+# Check what's running and stopped
+def get_status(ec2):
+
+    # Check running instances:
+    print "--- Running Instances ---"
+    running = ec2.running()
+    print(running)
+    print "--- Stopped Instances ---"
+    stopped = ec2.stopped()
+    print(stopped)
+
 if __name__ == "__main__":
+
+    # First start by checking the status of the instances and start them
     ec2 = EC2()
 
-    # VPCs
-    #VpcId = ec2.get_vpcs()['Vpcs'][0]['VpcId']
-    #ec2.get_security_groups()
-    print(ec2.get_security_group("sg-e5ad4e83"))
+    # Get the status
+    get_status(ec2)
+
+    # To stop an instance
+    #ec2.stop('i-0947507900f47f445')
+
+    # To see the existing VPCs and their status
+    print("--- VPCs and their status ---")
+    print(ec2.get_vpcs())
+    print("--- Security Groups ---")
+    print(ec2.get_security_groups())
+
+
+
+
+
+
