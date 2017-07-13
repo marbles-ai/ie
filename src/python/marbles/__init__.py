d00fa42c2786d0731dca35d6b8de4835d47d2dcf
Paul Glendenning <paul@marbles.ai>
2017-07-13 08:43:22 -0700

    Record str type, unicode or byte.

--- src/python/marbles/__init__.py
+++ src/python/marbles/__init__.py
@@ -2,7 +2,7 @@ import os
 
 PROJDIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 USE_DEVEL_PATH = os.path.exists(os.path.join(PROJDIR, 'src', 'python', 'marbles'))
-
+UNICODE_STRINGS = False
 
 def safe_utf8_encode(s):
     if isinstance(s, unicode):
