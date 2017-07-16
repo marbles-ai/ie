43415a2c678f33ce701f307dff81842bc9d2b4f5
Tommy Tracy II <tjt7a@virginia.edu>
2017-07-16 00:49:02 -0400

    Fixed bug and added print statements

--- site/deploy/deploy.py
+++ site/deploy/deploy.py
@@ -10,7 +10,7 @@ WARNING = 'WARNING: Are you sure you want to start AWS Service?'
 #   http://docs.aws.amazon.com/AmazonECS/latest/developerguide/ecs-optimized_AMI.html
 #   ami-7d664a1d
 
-# site_instance_id = 'asi instance id'
+site_instance_id = 'i-01b237a02cb5d2a24'
 # infox_instance_id = 'infox instance id'
 # ccg_instance_id = 'ccg instance id'
 # news_instance_id = 'news reader instance id'
@@ -36,16 +36,19 @@ if __name__ == "__main__":
             if str(raw_input(WARNING)) not in YES:
                 print("You decided not to start AWS services")
                 exit(0)
+            else:
+                print("Starting AWS services")
 
             # Start EC2 Instance
             ec2 = EC2()
-            ec2.start(site_instance_id)
+            print ec2.start(site_instance_id)
 
         elif task == 'stop':
 
+            print("Stopping AWS services")
             # Stop EC2 Instance
             ec2 = EC2()
-            ec2.stop(site_instance_id)
+            print ec2.stop(site_instance_id)
 
     # ---- For the infox service ---- #
 
@@ -56,15 +59,19 @@ if __name__ == "__main__":
             if str(raw_input(WARNING)) not in YES:
                 print("You decided not to start AWS services")
                 exit(0)
+            else:
+                print("Starting AWS services")
 
             # Start EC2 Instance
             ec2 = EC2()
-            ec2.start(infox_instance_id)
+            print ec2.start(infox_instance_id)
 
         elif task == 'stop':
 
+            print("Stopping AWS service")
+
             ec2 = EC2()
-            ec2.stop(infox_instance_id)
+            print ec2.stop(infox_instance_id)
 
     # ---- For the ccgparser ---- #
 
@@ -75,14 +82,18 @@ if __name__ == "__main__":
             if str(raw_input(WARNING)) not in YES:
                 print("You decided not to start AWS services")
                 exit(0)
+            else:
+                print("Starting AWS service")
 
             # Start EC2 Instance
             ec2 = EC2()
-            ec2.start(ccg_instance_id)
+            print ec2.start(ccg_instance_id)
 
         elif task == 'stop':
 
+            print("Stopping AWS service")
+
             ec2 = EC2()
-            ec2.stop(ccg_instance_id)
+            print ec2.stop(ccg_instance_id)
 
 
