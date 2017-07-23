a26f04a03160e610c5592e31f3b475b7cdd0300d
Tommy Tracy II <tjt7a@virginia.edu>
2017-07-22 20:24:47 -0400

    Added update task to each service; now builds and pushed updated image to registry! Tested with website and infox; testing others later

--- src/python/marbles/deploy/ECR.py
+++ src/python/marbles/deploy/ECR.py
@@ -1,7 +1,28 @@
 import boto3
+import subprocess
 
 
 class ECR:
 
     def __init__(self):
-        self.ec2 = boto3.client('ecr')
+        self.ecr = boto3.client('ecr')
+
+    def list_images(self, repository):
+        response = self.ecr.list_images(
+            repositoryName=repository
+        )
+        print "----- Available Images -----"
+        for image in response['imageIds']:
+            print image
+
+    def build_docker(self, image_name, root_directory):
+        subprocess.call(['docker', 'build', '-t', image_name,
+                        root_directory])
+
+    def tag_docker(self, tag, image):
+        subprocess.call(['docker', 'tag', tag, image])
+
+    def push_docker(self, image):
+        subprocess.call(['docker', 'push',
+                         image])
+
