042277c7dd93d19b00377852d83beb347f9cf30d
Paul Glendenning <paul@marbles.ai>
2017-07-27 23:48:29 -0700

    Update copyright.

--- src/main/java/ai/marbles/grpc/ServiceAcceptor.java
+++ src/main/java/ai/marbles/grpc/ServiceAcceptor.java
@@ -1,33 +1,7 @@
 /*
- * Copyright 2016 (c). All rights reserved.
+ * Copyright (c) Marbles AI Corp. 2016-2017.
+ * All rights reserved.
  * Author: Paul Glendenning
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met:
- *
- *    * Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer.
- *    * Redistributions in binary form must reproduce the above
- * copyright notice, this list of conditions and the following disclaimer
- * in the documentation and/or other materials provided with the
- * distribution.
- *
- *    * Neither the name of the author, nor the names of other
- * contributors may be used to endorse or promote products derived from
- * this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 package ai.marbles.grpc;
@@ -50,7 +24,7 @@ public class ServiceAcceptor {
 	private final int port_;
 	private final Server server_;
 
-	/** 
+	/**
 	  * Create a server listening on {@code port} using service to handle requests.
 	  *
 	  * @param  port    The port to listen on
@@ -78,7 +52,7 @@ public class ServiceAcceptor {
 		return server_;
 	}
 
-	/** 
+	/**
 	 * Start serving requests. Requests are handled in daemon threads.
 	 */
 	public void start() throws IOException {
@@ -95,7 +69,7 @@ public class ServiceAcceptor {
 		});
 	}
 
-	/** 
+	/**
 	 * Stop serving requests and shutdown resources.
 	 */
 	public ServiceAcceptor shutdown() {
