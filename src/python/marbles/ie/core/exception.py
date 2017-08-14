dd3ac45dca35864ce1933f49f7a4abd47c50954d
Paul Glendenning <paul@marbles.ai>
2017-08-13 18:34:01 -0700

    Exception module.

---
+++ src/python/marbles/ie/core/exception.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, print_function
+
+
+class DrsComposeError(Exception):
+    """DrsComposeError Error."""
+    pass
+
+
+class UnaryRuleError(Exception):
+    """UnaryRuleError Error."""
+    pass
+
