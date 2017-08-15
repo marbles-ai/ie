0b0ecfe35126defa21b2e67b3485dfc879fc9ef4
Paul Glendenning <paul@marbles.ai>
2017-08-15 12:24:00 -0700

    Move undefined template tracking here.

--- src/python/marbles/ie/core/exception.py
+++ src/python/marbles/ie/core/exception.py
@@ -3,11 +3,41 @@ from __future__ import unicode_literals, print_function
 
 
 class DrsComposeError(Exception):
-    """DrsComposeError Error."""
+    """Drs Composition Error."""
     pass
 
 
 class UnaryRuleError(Exception):
-    """UnaryRuleError Error."""
+    """Unary Rule Error."""
     pass
+
+
+class TemplateRuleError(Exception):
+    """Functor Template Rule Error"""
+    pass
+
+# Track missing rules
+_UNDEFINED_UNARY = set()
+_UNDEFINED_TEMPLATES = set()
+
+
+def save_undefined_unary_rules(path):
+    """Save missing unary rules to a file."""
+    if len(_UNDEFINED_UNARY) == 0:
+        return
+    with open(path, 'w') as fp:
+        for rule in _UNDEFINED_UNARY:
+            fp.write('%s %s\n' % rule)
+
+
+def save_undefined_template_rules(path):
+    """Save missing unary rules to a file."""
+    if len(_UNDEFINED_TEMPLATES) == 0:
+        return
+    with open(path, 'w') as fp:
+        for rule in _UNDEFINED_TEMPLATES:
+            fp.write('%s %s\n' % rule)
+
+
+
 
